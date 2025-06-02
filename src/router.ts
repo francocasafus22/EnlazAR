6c12dee8b7699b3e8a948e49f982cc6cb4d167fc
francocasafus22 <francocasafus55@gmail.com>
2025-06-02 02:19:08 -0300

    add mongodb connection, user register and use scheme and model

--- src/router.ts
+++ src/router.ts
@@ -1,15 +1,23 @@
-import { log } from "console";
 import { Router } from "express";
+import User from "./models/User";
 
 const router = Router();
 
+router.get("/", (req, res) => {
+  res.send("Mi primer server con TypeScript y Express :)");
+});
+
 // Autenticacion y registro
 
-router.post("/auth/register", (req, res) => {
-  const login = req.body;
-  console.log(login);
-  res.send(`Hola ${login.user}`);
+router.post("/auth/register", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).json({ message: "Usuario creado", user });
+  } catch (error) {
+    console.error("❌ Error al registrar:", error);
+    res.status(500).json({ error: "Error en el servidor" });
+  }
 });
 
 export default router;
- 
